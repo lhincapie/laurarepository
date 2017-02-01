bb4758079faf7e1e7faa1aa1b50d7c8ea03bc1c6
lhincapie <laura-hincapie@hotmail.com>
2017-01-31 20:15:44 -0500

    intial commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package estilolau
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'estilo' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'estilo' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'estilo' ), 'estilo', '<a href="https://automattic.com/" rel="designer">laurahincapie</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
